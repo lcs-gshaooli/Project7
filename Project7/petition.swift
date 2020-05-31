07c3d5d3dce29cc4d58c0af7d38a13bffad04355
Gabriela Shaooli <gshaooli@lcs.on.ca>
2020-05-31 16:22:33 -0500

    Petitions, parsing Json.

---
+++ Project7/petition.swift
@@ -0,0 +1,14 @@
+//
+//  petition.swift
+//  Project7
+//
+//  Created by Gabriela Shaooli on 2020-05-31.
+//  Copyright © 2020 Gabriela Shaooli. All rights reserved.
+//
+
+import Foundation
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/petitions.swift
@@ -0,0 +1,12 @@
+//
+//  petitions.swift
+//  Project7
+//
+//  Created by Gabriela Shaooli on 2020-05-31.
+//  Copyright © 2020 Gabriela Shaooli. All rights reserved.
+//
+
+import Foundation
+struct Petitions: Codable {
+    var results: [Petition]
+}
